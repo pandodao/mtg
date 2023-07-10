33af342f602c779ba4fae2df6b41dcb2476b3631
yiplee <guoyinl@gmail.com>
2023-07-10 11:10:21 +0800

    add test

--- protocol/checksum/sha256.go
+++ protocol/checksum/sha256.go
@@ -1,16 +1,18 @@
-package protocol
+package checksum
 
 import (
 	"bytes"
 	"crypto/sha256"
 )
 
+// Sha256 returns the first 4 bytes of the sha256 checksum of data.
 func Sha256(data []byte) []byte {
 	b := sha256.Sum256(data)
 	b = sha256.Sum256(b[:])
 	return b[:4]
 }
 
+// Sha256Verify returns the body and whether the checksum of data is correct.
 func Sha256Verify(data []byte) ([]byte, bool) {
 	if len(data) < 4 {
 		return nil, false
